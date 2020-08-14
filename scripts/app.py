acb56ef75afcd9ea99871821ac8f79949a47d710
Mehdi Rezaie <medirz90@icloud.com>
2020-08-14 12:18:27 -0400

    clean up cmd argument parser

--- scripts/app.py
+++ scripts/app.py
@@ -7,21 +7,16 @@
         - Add a yaml file to use for the inputs, useful for scaling to 1k mocks
         - with feature selection the shape of the input layer is different, the model cannot be restored
 """
-import os
-import sys
-import argparse
+
 
 import sysnet
 
 import torch
 torch.autograd.set_detect_anomaly(True) # check 
 
-ap = argparse.ArgumentParser()
-config = sysnet.parse_cmd_arguments(ap, 'config.yaml')  # read input parameters from the command line
-
+config = sysnet.parse_cmd_arguments('config.yaml')  # read input parameters from the command line
 
 # preprocess (read data, randoms, & templates, prepare tabulated data for training)
-
 # modeling (feature selection and regression
 pipeline = sysnet.SYSNet(config)
 pipeline.run()
--- sysnet/cli.py
+++ sysnet/cli.py
@@ -1,112 +1,117 @@
+import argparse
+
 from sysnet.sources.io import Config
+
+
 __all__ = ['parse_cmd_arguments']
 
-def parse_cmd_arguments(parser, yaml_config=None):    
+def parse_cmd_arguments(yaml_config=None):    
     ''' command argument parser
     '''
     cf = Config(yaml_config)
-            
-    parser.add_argument('-i', '--input_path',
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('-i', '--input_path',
                         type=str,
                         default=cf.fetch('input_path', '../input/eBOSS.ELG.NGC.DR7.table.fits'),
                         help='path to the input data')
 
-    parser.add_argument('-o', '--output_path',
+    ap.add_argument('-o', '--output_path',
                         type=str,
                         default=cf.fetch('output_path', '../output/model_test'),
                         help='path to the output')
 
-    parser.add_argument('--restore_model',
+    ap.add_argument('--restore_model',
                         type=str,
                         default=cf.fetch('restore_model', None),
                         help='model to restore the weights from')
 
-    parser.add_argument('-bs', '--batch_size',
+    ap.add_argument('-bs', '--batch_size',
                         type=int,
                         default=cf.fetch('batch_size', 4098),
                         help='minibatch size')
 
-    parser.add_argument('-ne', '--nepochs',
+    ap.add_argument('-ne', '--nepochs',
                         type=int,
                         default=cf.fetch('nepochs', 1),
                         help='number of training epochs')
 
-    parser.add_argument('-nc', '--nchains',
+    ap.add_argument('-nc', '--nchains',
                         type=int,
                         default=cf.fetch('nchains', 1),
                         help='number of chains with different initializations')
 
-    parser.add_argument('-fl', '--find_lr',
+    ap.add_argument('-fl', '--find_lr',
                         default=cf.fetch('find_lr', False),
                         action='store_true',
                         help='find the best learning rate')
 
-    parser.add_argument('-fs', '--find_structure',
+    ap.add_argument('-fs', '--find_structure',
                         default=cf.fetch('find_structure', False),
                         action='store_true',
                         help='find the best nn structure')
 
-    parser.add_argument('-fl1', '--find_l1',
+    ap.add_argument('-fl1', '--find_l1',
                         default=cf.fetch('find_l1', False),
                         action='store_true',
                         help='find the best L1 alpha')
 
-    parser.add_argument('-k', '--do_kfold',
+    ap.add_argument('-k', '--do_kfold',
                         default=cf.fetch('do_kfold', False),
                         action='store_true',
                         help='enable k-fold cross validation (k=5)')
 
-    parser.add_argument('-norm', '--normalization',
+    ap.add_argument('-norm', '--normalization',
                         type=str,
                         default=cf.fetch('normalization', 'z-score'), 
                         help='standardization method')
 
-    parser.add_argument('--model',
+    ap.add_argument('--model',
                         type=str,
                         default=cf.fetch('model', 'dnnp'),
                         help='model, dnn or dnnp')
-    
-    parser.add_argument('--optim',
+
+    ap.add_argument('--optim',
                         type=str,
                         default=cf.fetch('optim', 'adamw'),
                         help='adamw or sgd')
 
-    parser.add_argument('-ax', '--axes',
+    ap.add_argument('-ax', '--axes',
                         type=int,
                         nargs='*',
                         default=cf.fetch('axes', [0]),
                         #required=True,
                         help='index of features to use')
 
-    parser.add_argument('--do_rfe',
+    ap.add_argument('--do_rfe',
                         default=cf.fetch('do_rfe', False),
                         action='store_true',
                         help='perform recursive feature elimination')
 
-    parser.add_argument('--eta_min',
+    ap.add_argument('--eta_min',
                         type=float,
                         default=cf.fetch('eta_min', 1.0e-5),
                         help='min eta for LR finder')
 
-    parser.add_argument('-lr', '--learning_rate',
+    ap.add_argument('-lr', '--learning_rate',
                         type=float,
                         default=cf.fetch('learning_rate', 1.0e-3),
                         help='best initial learning rate')
 
-    parser.add_argument('--nn_structure',
+    ap.add_argument('--nn_structure',
                         type=int,
                         default=cf.fetch('nn_structure', (4, 20)),
                         nargs='*',
                         help='structure ( # hidden layer, # neurons)')
 
-    parser.add_argument('--l1_alpha',
+    ap.add_argument('--l1_alpha',
                         type=float,
                         default=cf.fetch('l1_alpha', -1.0),
                         help='L1 scale (negative value will turn off regularization)')
 
-    parser.add_argument('--loss',
+    ap.add_argument('--loss',
                         type=str,
                         default=cf.fetch('loss', 'pnll'),
                         help='Cost function (loss) e.g., mse, pnll')
 
-    return parser.parse_args()
+    return ap.parse_args()
